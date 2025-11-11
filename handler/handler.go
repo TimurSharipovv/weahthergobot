42e7adaf08b642aa4059000f83aab9d1746e8d62
Anywhay <timursaripov03480@gmail.com>
2025-11-11 14:26:46 +0300

    v3.0

--- handler/handler.go
+++ handler/handler.go
@@ -28,36 +28,39 @@ func (handler *Handler) Start() {
 	updates := handler.bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		handler.HandlerUpdate(update)
+		handler.handlerUpdate(update)
 	}
 }
 
-func (handler *Handler) HandlerUpdate(update tgbotapi.Update) {
-	if update.Message != nil { // If we got a message
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-		coordinates, err := handler.owClient.Coordinates(update.Message.Text)
-		if err != nil {
-			log.Println(err)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "не смогли получить координаты")
-			msg.ReplyToMessageID = update.Message.MessageID
-			handler.bot.Send(msg)
-			return
-		}
-
-		weather, err := handler.owClient.Weather(coordinates.Lat, coordinates.Lon)
-		if err != nil {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "не смогли получить прогноз погоды в этой местности")
-			msg.ReplyToMessageID = update.Message.MessageID
-			handler.bot.Send(msg)
-			return
-		}
-
-		msg := tgbotapi.NewMessage(
-			update.Message.Chat.ID,
-			fmt.Sprintf("Температура в %s: %d градусов", update.Message.Text, int(math.Round(weather.Temp))),
-		)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		handler.bot.Send(msg)
+func (handler *Handler) handlerUpdate(update tgbotapi.Update) {
+	if update.Message == nil {
+		return
 	}
+	// If we got a message
+	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	coordinates, err := handler.owClient.Coordinates(update.Message.Text)
+	if err != nil {
+		log.Println(err)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "не смогли получить координаты")
+		msg.ReplyToMessageID = update.Message.MessageID
+		handler.bot.Send(msg)
+		return
+	}
+
+	weather, err := handler.owClient.Weather(coordinates.Lat, coordinates.Lon)
+	if err != nil {
+		log.Println(err)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "не смогли получить прогноз погоды в этой местности")
+		msg.ReplyToMessageID = update.Message.MessageID
+		handler.bot.Send(msg)
+		return
+	}
+
+	msg := tgbotapi.NewMessage(
+		update.Message.Chat.ID,
+		fmt.Sprintf("Температура в %s: %d градусов", update.Message.Text, int(math.Round(weather.Temp))),
+	)
+	msg.ReplyToMessageID = update.Message.MessageID
+
+	handler.bot.Send(msg)
 }
